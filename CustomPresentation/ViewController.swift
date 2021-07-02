53da428b422ddeeb049152fb560657c9c74b8901
DonghoChoi <pridesam@snu.ac.kr>
2021-07-02 21:33:31 +0900

    Initial Commit

---
+++ CustomPresentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomPresentation
+//
+//  Created by 최동호 on 2021/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
